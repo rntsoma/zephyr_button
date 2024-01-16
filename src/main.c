f6b2b0edb57525eef12b01ebdb674953bf076672
Renato Soma <renatoys08@gmail.com>
2024-01-16 17:45:24 -0300

    add button

--- src/main.c
+++ src/main.c
@@ -2,16 +2,29 @@
 #include <stdbool.h>
 #include <zephyr/device.h>
 #include <zephyr/kernel.h>
+#include <zephyr/input/input.h>
 #include <zephyr/drivers/gpio.h>
 
 #if !DT_NODE_EXISTS(DT_PATH(leds))
 #error "No suitable devicetree overlay specified"
 #endif
+#if !DT_NODE_EXISTS(DT_PATH(buttons))
+#error "No suitable devicetree overlay specified"
+#endif
 #define LED0_NODE DT_ALIAS(led0)
 #define LED1_NODE DT_ALIAS(led1)
+#define BUTTON_NODE DT_ALIAS(button0)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led_proto = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
+
+static void button_handler(struct input_event *evt) {
+    gpio_pin_toggle_dt(&led);
+    gpio_pin_toggle_dt(&led_proto);
+}
+
+INPUT_CALLBACK_DEFINE(NULL, button_handler);
 
 int main(void) {
     int err;
@@ -30,13 +43,18 @@ int main(void) {
 		return 0;
 	}
 
+	err = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (err < 0) {
+		return 0;
+	}
+
     while (true) {
 		/* BEGIN */
 		/* END */
 
-        gpio_pin_toggle_dt(&led);
-        gpio_pin_toggle_dt(&led_proto);
-        k_msleep(2000);
+        /* gpio_pin_toggle_dt(&led); */
+        /* gpio_pin_toggle_dt(&led_proto); */
+        /* k_msleep(2000); */
 }
 
     return 0;
